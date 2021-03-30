cb45a8b66fafc0901dff6f7293733f19f2f96501
bastolatanuja <tanujabastola143@gmail.com>
2021-03-30 11:34:35 +0545

    price of the house

---
+++ lab exercise 2/question no 2.py
@@ -0,0 +1,10 @@
+#price of a house is $1M.if buyer has good credit,they need to put dowm 10% otherwise they need to put down 20%.
+# print the payment.
+'''price=100000
+has_good_credit=True
+if has_good_credit:
+    down_payment= 0.1 * price
+else:
+    down_payment= 0.2 * price
+print(f"Down payment: ${down_payment}")
+
