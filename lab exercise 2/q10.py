6e4cfad78458dc87c3d37d02e1d3d116a1a4923a
bastolatanuja <tanujabastola143@gmail.com>
2021-04-06 12:12:28 +0545

    integer

---
+++ lab exercise 2/q10.py
@@ -0,0 +1,8 @@
+#given three integers ,print the smallest one.(three integers should be user input)
+num1=int(input("enter the number: "))
+num2=int(input("enter the second number: "))
+num3=int(input("enter the third number: "))
+if num1<num2:
+    print(f"the {num1} is the smallest")
+elif num2<num3:
+    print(f"the {num2}is the smallest")
