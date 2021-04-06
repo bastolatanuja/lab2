b695ac3611cd57f537477459b93aac24a092a039
bastolatanuja <tanujabastola143@gmail.com>
2021-04-06 12:04:54 +0545

    odd and even

---
+++ lab exercise 2/question no.9.py
@@ -0,0 +1,6 @@
+#check whether the user input number is even or odd and display it to user.
+given_number= int(input("enter the number: "))
+if given_number%2==0:
+    print("the number is even")
+else:
+    print("the numbwr is odd")
